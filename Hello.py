152a57d0d0d3345d9dd386e4a74023f7b126138a
ladanyuwu <146982312+ladanyuwu@users.noreply.github.com>
2023-10-04 11:40:00 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.ndimage.interpolation import zoom
 from streamlit_drawable_canvas import st_canvas
 from utils import process_image
-st.markdown("# Aplicación de :blue[Reconocimiento] :green[de digitos] :pencil: 💻🤖")
+st.markdown("# Aplicación de :pink[Reconocimiento] :purple[de digitos] :pencil: 💻🤖")
 
 # Load trained model
 model = tf.keras.models.load_model('mi_modelo.h5')
